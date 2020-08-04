c97b1c060f0574b55eed42b5e76d804e7cb9af04
kimjaemin <ansy6969@gmail.com>
2020-08-04 17:19:23 +0900

    add recommend revise project

--- aptamer/urls.py
+++ aptamer/urls.py
@@ -5,4 +5,5 @@ from. import views
 app_name = 'aptamer'
 urlpatterns = [
     path('recommend/', views.IndexView.as_view(), name='index'),
+    path('recommend/success/', views.IndexView.as_view(), name='success'),
 ]
--- aptamer/views.py
+++ aptamer/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from django.template.loader import get_template
+from .forms import UploadFileForm
 
 
 class IndexView(generic.DetailView):
@@ -9,3 +10,13 @@ class IndexView(generic.DetailView):
     def get(self, request, *args, **kwargs):
         ctx = {}  # 템플릿에 전달할 데이터
         return self.render_to_response(ctx)
+
+    def upload_File(request):
+        if request.method == 'POST':
+            form = UploadFileForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect("{% url aptamer:success %}")
+        else:
+            form = UploadFileForm()
+        return render(request, 'upload.html', {'form': form})
